71b32846721d561bf041d2cfdeca4a9d1657a9ed
Dylan Lotz <dschnake09@yahoo.com>
2021-01-13 17:38:06 -0600

    Working on studio 3.

--- src/org/launchcode/java/studios/restaurantmenu/Menu.java
+++ src/org/launchcode/java/studios/restaurantmenu/Menu.java
@@ -1,5 +1,50 @@
 package org.launchcode.java.studios.restaurantmenu;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
 
 public class Menu {
+    public static void main(String[] args){
+        ArrayList<String> foodName = new ArrayList<>();
+        ArrayList<Double> foodPrice = new ArrayList<>();
+        ArrayList<String> foodDescription = new ArrayList<>();
+        ArrayList<String> foodCategory = new ArrayList<>();
+        ArrayList<Calendar> updateDate = new ArrayList<>();
 
+        Scanner input = new Scanner(System.in);
+        String newFoodName;
+
+        System.out.println("Enter the name of the new menu item (or press ENTER to finish):");
+
+        do {
+            newFoodName = input.nextLine();
+
+            if (!newFoodName.equals("")) {
+                foodName.add(newFoodName);
+            }
+
+        } while (!newFoodName.equals(""));
+
+        for (int i = 0; i < foodName.size(); i++){
+            System.out.println("Enter a price for " + foodName.get(i) + ": ");
+            Double newPrice = input.nextDouble();
+            foodPrice.add(newPrice);
+        }
+
+        for (int i = 0; i < foodName.size(); i++){
+            System.out.println("Enter a description for " + foodName.get(i) + ": ");
+            String newDescription = input.nextLine();
+            foodDescription.add(newDescription);
+        }
+
+        for (int i = 0; i < foodName.size(); i++){
+            System.out.println("Please choose a category for " + foodName.get(i) + " from the following list: Appetizer, Main Course, Dessert:");
+            String newCategory = input.nextLine();
+            foodCategory.add(newCategory);
+        }
+
+        for (int i = 0; i < foodName.size(); i++) {
+            System.out.println("Menu item: " + foodName.get(i) + " Price: " + foodPrice.get(i) + " Description: " + foodDescription.get(i) + " Category: " + foodCategory.get(i));
+        }
+    }
 }
